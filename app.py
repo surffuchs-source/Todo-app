563bfbb9cb8c5d2539479417c25934f972675167
surffuchs-source <surffuchs@googlemail.com>
2026-06-17 13:30:53 +0000

    Fix db init on startup

--- app.py
+++ app.py
@@ -105,3 +105,7 @@ def delete_todo(todo_id):
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+# Initialize DB on startup
+with app.app_context():
+    init_db()
